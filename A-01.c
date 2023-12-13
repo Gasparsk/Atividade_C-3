13fc2b6949c9bcc0c0b55a21f4a134969f835f14
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2023-12-12 21:46:42 -0300

    A-01.c

---
+++ A-01.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 3
+#include <string.h>
+#include <time.h>
+
+
+struct dados_pessoa
+{
+    char nome[200];
+    char telefone[10];
+    char email[200];
+};
+
+char *verificacaoDados(struct dados_pessoa contato[], char *buscador)
+{
+    int i;
+
+    for (i = 0; i < TAM; i++)
+    {
+
+        if (strcmp(contato[i].nome, buscador) == 0)
+        {
+            printf("Numero encontrado: %s \n", contato[i].telefone);
+            break;
+        }
+        printf("\nContato não encontrado.");
+    }
+}
+
+int main()
+{
+    int i;
+    char encontrar[200];
+
+    struct dados_pessoa contato[TAM];
+
+    for (i = 0; i < TAM; i++)
+    {
+        printf("Digite seu nome: \n");
+        gets(contato[i].nome);
+
+        printf("Digite seu telefone: \n");
+        gets(contato[i].telefone);
+
+        printf("Digite seu e-mail: \n");
+        gets(contato[i].email);
+
+        printf("\n");
+    }
+
+    printf("Busca de contatos.\n");
+    printf("Digite um nome para acessar o número de contato: ");
+    gets(buscador);
+
+    verificacaoDados(contato,encontrar);
+
+    return 0;
+}
